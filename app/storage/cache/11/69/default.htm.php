24d53c6e74372edf1bb798906fb9fa706c387203
subekti cahyo <subekti.cahyo@gmail.com>
2014-10-05 10:43:03 -0700

    initial commit

---
+++ app/storage/cache/11/69/default.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms54318238e6799_2093131402Class extends \Cms\Classes\LayoutCode
+{
+
+}
---
+++ app/storage/cache/11/78/home.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms54318238ea3d7_369387318Class extends \Cms\Classes\PageCode
+{
+
+}
